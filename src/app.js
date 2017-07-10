dbe8c54f5548e5c7bd72d6f04e255e4476bebc96
GitNiko <galaxis.ling@gmail.com>
2017-07-10 14:23:53 +0800

    use redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+
+const store = configureStore();
 
 export default class App extends React.Component {
   render() {
-    return <div>start navigation</div>;
+    return (
+      <Provider store={store}>
+        <div>start navigation</div>
+      </Provider>
+    );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const naviReducer = (state = {}, action) => state;
+
+export default combineReducers({
+  navi: naviReducer
+});
